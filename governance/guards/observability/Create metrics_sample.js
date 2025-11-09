813964b00b2648b4e2ba7d7d7097cd9a877a6a2f
truthsealai-spec <truthseal.ai@gmail.com>
2025-11-09 16:51:20 +1100

    governance/guards/observability/Create metrics_sample.js: remove wrong sample file

--- governance/guards/observability/Create metrics_sample.js
+++
@@ -1,8 +0,0 @@
-{
-  "EVL_Hash_Mismatch_Count": 0,
-  "Commander_Output_Redaction_Rate": 0.00,
-  "Blocked_HighRisk_Jobs": 3,
-  "Detected_HighRisk_Jobs": 5,
-  "Reversal_Edits_Rate": 0,
-  "Missing_Breadcrumbs_Rate": 0
-}
